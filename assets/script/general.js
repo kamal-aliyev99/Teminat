a00790e99ccfd8a0fb2632ebd16b011a2f565697
mirikerimli <mirkerimgubadov@gmail.com>
2024-07-13 03:01:19 +0400

    about section is completed, scroll to top button is already created

--- assets/script/general.js
+++ assets/script/general.js
@@ -1,8 +1,26 @@
 const openMobNavBtn = document.getElementById("openMobNav");
 const mobileNavbar = document.getElementById("mobileNav");
+const scroll_top = document.querySelector("#scroll_top");
 
 function openMobileNav() {
   mobileNavbar.classList.toggle("active_mobile_nav");
 }
 
 openMobNavBtn.onclick = openMobileNav;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY < 120) {
+    scroll_top.style.display = "none";
+  } else {
+    scroll_top.style.display = "inline-block";
+  }
+});
+
+scroll_top.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
